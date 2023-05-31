ce8e3c599cda13f982eedb4a82fd6826ea724f93
y-brehm <yannikbrehm@web.de>
2023-05-31 15:13:43 +0200

    add plugins.lua

---
+++ .config/nvim/lua/yannikbrehm/plugins.lua
@@ -0,0 +1,55 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({ "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+vim.g.mapleader = " "
+
+require("lazy").setup(
+{
+    'nvim-treesitter/nvim-treesitter',
+    'navarasu/onedark.nvim',
+    'nvim-telescope/telescope.nvim',
+    'jvgrootveld/telescope-zoxide',
+    'nvim-lua/plenary.nvim',
+    'VonHeikemen/lsp-zero.nvim',
+    'neovim/nvim-lspconfig',
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'nvim-tree/nvim-tree.lua',
+    'nvim-tree/nvim-web-devicons',
+    'folke/todo-comments.nvim',
+    'folke/twilight.nvim',
+    'iamcco/markdown-preview.nvim',
+    'lewis6991/gitsigns.nvim',
+    'Pocco81/auto-save.nvim',
+    'moll/vim-bbye',
+    'akinsho/bufferline.nvim',
+    -- autocompletion
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-nvim-lua',
+    'rafamadriz/friendly-snippets',
+    'onsails/lspkind.nvim',
+    'github/copilot.vim',
+    'windwp/nvim-autopairs', -- Autopairs, integrates with cmp
+    -- debugging
+    'mfussenegger/nvim-dap',
+    'rcarriga/nvim-dap-ui',
+    'theHamsta/nvim-dap-virtual-text',
+    'mfussenegger/nvim-dap-python'
+})
+
+require('onedark').load()
+require('todo-comments').setup()
+require('twilight').setup()
+require('gitsigns').setup()
+require('mason').setup()
