9375a1a4d169d08f93b32765fdec341ac80b4974
y-brehm <yannikbrehm@web.de>
2023-07-31 16:02:32 +0200

    fix high cpu flake8 error

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -3,11 +3,7 @@ local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.black,
-        null_ls.builtins.diagnostics.flake8.with({
-            command = "flake8",
-            args = { "--max-line-length=100" },
-        }),
-        null_ls.builtins.completion.spell,
+        null_ls.builtins.diagnostics.flake8.with({extra_args = {"--max-line-length", "100"}}),
     },
 })
 
