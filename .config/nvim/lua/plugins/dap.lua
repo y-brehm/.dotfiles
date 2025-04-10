9c0ae0c3880d9f3f316699680efb924610b5f143
y-brehm <y_brehm@proton.me>
2025-04-10 18:24:43 +0200

    add function to debug specific python file

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,3 +1,39 @@
+local function select_python_file()
+  -- Use vim.ui.select to choose a file
+  local files = vim.fn.glob(vim.fn.getcwd() .. '/**/*.py', false, true)
+  vim.ui.select(files, {
+    prompt = "Select Python file to debug: ",
+    format_item = function(item)
+      -- Convert absolute path to relative for cleaner display
+      return vim.fn.fnamemodify(item, ":.")
+    end,
+  }, function(file)
+    if file then
+      -- Store selected file in a global variable
+      vim.g.dap_python_selected_file = file
+      -- Run the debugger with this file
+      require('dap').run({
+        type = "python",
+        request = "launch",
+        name = "Launch selected file",
+        program = file,
+        pythonPath = function()
+          if vim.fn.filereadable('.venv/bin/python') == 1 or vim.fn.filereadable('.venv/Scripts/python.exe') == 1 then
+            if vim.fn.has('win32') == 1 then
+              return vim.fn.getcwd() .. '\\.venv\\Scripts\\python.exe'
+            else
+              return vim.fn.getcwd() .. '/.venv/bin/python'
+            end
+          else
+            return python_path
+          end
+        end,
+      })
+    end
+  end)
+end
+
+
 return {
   {
     "mfussenegger/nvim-dap",
@@ -65,10 +101,11 @@ return {
       require("nvim-dap-virtual-text").setup()
 
     local wk = require("which-key")
-
+    vim.api.nvim_create_user_command('DapPythonPickFile', select_python_file, {})
     wk.add({
         { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = '[D]ebug [B]reakpoint' },
         { "<leader>dc", function() require("dap").continue() end, desc = '[D]ebug [C]ontinue' },
+        { "<leader>dpf", ":DapPythonPickFile<CR>", desc = "[D]ebug [P]ython [F]ile selector" },
         { "<leader>dso", function() require("dap").step_over() end, desc = '[D]ebug [S]tep [O]ver' },
         { "<leader>dsi", function() require("dap").step_in() end, desc = '[D]ebug [S]tep [I]nto' },
         { "<leader>dt", function() require("dap").test_method() end, desc = '[D]ebug [T]est' },
@@ -100,7 +137,7 @@ return {
           {
             type = "python",
             request = "launch",
-            name = "Launch file",
+            name = "Launch current file",
             program = "${file}",
             pythonPath = function()
               -- Try to detect the environment python
@@ -137,5 +174,8 @@ return {
       end,
     },
   },
+
 }
+
 }
+
