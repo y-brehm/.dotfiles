62a778c63a54954099dc0db5ce0e8674fe2f8433
y-brehm <y_brehm@proton.me>
2025-11-14 17:53:45 +0100

    fix cmake-tools issues on windows

--- .config/nvim/lua/plugins/cmake-tools.lua
+++ .config/nvim/lua/plugins/cmake-tools.lua
@@ -7,14 +7,9 @@ return {
     "akinsho/toggleterm.nvim",
   },
   opts = {
-    cmake_build_directory = function()
-    local osys = require("cmake-tools.osys")
-      if osys.iswin32 then
-        return "build\\${variant:buildType}"
-      end
-      return "build/${variant:buildType}"
-    end,
+    cmake_build_directory = "build",
     cmake_generate_compile_commands = true,
+    cmake_use_preset = "auto",
     cmake_configure_on_edit = false,
     cmake_configure_on_variant_change = false,
     cmake_executor = { -- executor to use
@@ -75,6 +70,58 @@ return {
 
   config = function(_, opts)
     require("cmake-tools").setup(opts)
+
+    -- Workaround for Windows path issues with cmake presets
+    local is_windows = vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1
+
+    -- Clean build directory completely
+    local function clean_build_dir()
+      local cmt = require("cmake-tools")
+      local build_dir = cmt.get_build_directory()
+      if build_dir then
+        local dir_str = tostring(build_dir)
+        if is_windows then
+          os.execute('rmdir /S /Q "' .. dir_str:gsub('/', '\\') .. '" 2>nul')
+        else
+          os.execute('rm -rf "' .. dir_str .. '"')
+        end
+        vim.notify("Cleaned build directory: " .. dir_str, vim.log.levels.INFO)
+      end
+    end
+
+    -- Ensure build directory exists before CMake runs
+    local function ensure_build_dir()
+      local cmt = require("cmake-tools")
+      local build_dir = cmt.get_build_directory()
+      if build_dir then
+        local dir_str = tostring(build_dir)
+        if is_windows then
+          os.execute('mkdir "' .. dir_str:gsub('/', '\\') .. '" 2>nul')
+        else
+          os.execute('mkdir -p "' .. dir_str .. '"')
+        end
+      end
+    end
+
+    -- Override CMakeGenerate to ensure directory exists first
+    -- With bang (!), clean build directory completely before generating
+    vim.api.nvim_create_user_command('CMakeGenerate', function(cmd_opts)
+      if cmd_opts.bang then
+        clean_build_dir()
+      end
+      ensure_build_dir()
+      require("cmake-tools").generate({}, function() end)
+    end, { bang = true, nargs = '?' })
+
+    -- Override CMakeBuild to ensure directory exists first
+    -- With bang (!), clean build directory completely before building
+    vim.api.nvim_create_user_command('CMakeBuild', function(cmd_opts)
+      if cmd_opts.bang then
+        clean_build_dir()
+      end
+      ensure_build_dir()
+      require("cmake-tools").build({}, function() end)
+    end, { bang = true, nargs = '?' })
 
     local wk = require("which-key")
     wk.add({
